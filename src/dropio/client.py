8b9d762beda359f98e156f01fa5f5dcb88cefb83
Jimmy Orr <jimmyorr@gmail.com>
2009-03-26 13:16:10 +0000

    added assertions, placeholders for exception handling in urlopen calls.  Filled out call to Asset constructor with basics, though they currently don't match what's coming back from the API

--- src/dropio/client.py
+++ src/dropio/client.py
@@ -1,5 +1,5 @@
 from StringIO import StringIO
-import simplejson
+import simplejson as json
 import urllib
 import urllib2
 
@@ -29,17 +29,34 @@ class DropIoClient(object):
         self.urlencoded_params = urllib.urlencode(params)
     
     def get_asset_list(self, drop_name):
-        # todo: paginate through asset list for > 30 assets
+        assert drop_name is not None
+        
+        # TODO: paginate through asset list for > 30 assets
         url = BASE_URL + drop_name + ASSETS + '?' + self.urlencoded_params
-        f = urllib2.urlopen(url)
-        json_encoded = StringIO(f.read())
-        asset_list = simplejson.load(json_encoded)
+        try:
+            f = urllib2.urlopen(url)
+            json_encoded = StringIO(f.read())
+            asset_list = json.load(json_encoded)
+        except IOError:
+            raise
+        
         for asset_dict in asset_list:
-            yield Asset(asset_dict['title'])
+            # TODO: JSON returned does not match documentation at
+            # http://groups.google.com/group/dropio-api/web/resource-descriptions
+            yield Asset(asset_dict.get('name'), asset_dict.get('type'),
+                        asset_dict.get('title'), asset_dict.get('description'),
+                        asset_dict.get('filesize'), asset_dict.get('created_at')
+                        )
     
     def get_drop(self, drop_name):
+        assert drop_name is not None
+        
         url = BASE_URL + DROPS + drop_name + '?' + self.urlencoded_params
-        f = urllib2.urlopen(url)
-        json_encoded = StringIO(f.read())
-        drop_dict = simplejson.load(json_encoded)
+        try:
+            f = urllib2.urlopen(url)
+            json_encoded = StringIO(f.read())
+            drop_dict = json.load(json_encoded)
+        except IOError:
+            raise
+        
         return Drop(drop_dict['url'], drop_dict['id'])
