3e89ea38a06098435ea3dcd60140897671ae5add
Jimmy Orr <jimmyorr@gmail.com>
2009-03-26 13:13:43 +0000

    Added fields for Asset resource based on documentation

--- src/dropio/resource.py
+++ src/dropio/resource.py
@@ -14,9 +14,22 @@ class Drop(Resource):
 
 
 class Asset(Resource):
-    def __init__(self, title):
+    """ 
+    Based on http://groups.google.com/group/dropio-api/web/resource-descriptions
+    (wrong)
+    """
+    def __init__(self, name, type, title, description, filesize, created_at,
+                 thumbnail=None, status=None, file=None, converted=None, 
+                 hidden_url=None, pages=None, duration=None, artist=None, 
+                 track_title=None, height=None, width=None, contents=None, 
+                 url=None):
         Resource.__init__(self)
+        self.name = name
+        self.type = type
         self.title = title
+        self.description = description
+        self.filesize = filesize
+        self.created_at = created_at
     
     def __str__(self):
         return self.title
