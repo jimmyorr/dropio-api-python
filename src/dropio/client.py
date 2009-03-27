994f09b2c6d8f851764e96acdfc4593bebb32b8b
Jimmy Orr <jimmyorr@gmail.com>
2009-03-27 03:46:27 +0000

    Added additional fields

--- src/dropio/client.py
+++ src/dropio/client.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Based on http://groups.google.com/group/dropio-api/web/full-api-documentation
+"""
+
+__author__ = 'jimmyorr@gmail.com (Jimmy Orr)'
+__version__ = '$Id$'
+
 from StringIO import StringIO
-import simplejson as json
 import urllib
 import urllib2
 
-from resource import Asset
-from resource import Drop
+import simplejson as json
+
+from resource import Asset, Drop
 
 API_VERSION = '1.0'
 API_FORMAT = 'json'
@@ -29,34 +38,52 @@ class DropIoClient(object):
         self.urlencoded_params = urllib.urlencode(params)
     
     def get_asset_list(self, drop_name):
+        """
+        Returns:
+            list of dropio.resource.Asset
+        """
         assert drop_name is not None
         
         # TODO: paginate through asset list for > 30 assets
         url = BASE_URL + drop_name + ASSETS + '?' + self.urlencoded_params
-        try:
-            f = urllib2.urlopen(url)
-            json_encoded = StringIO(f.read())
-            asset_list = json.load(json_encoded)
-        except IOError:
-            raise
+        f = self.__urlopen(url)
         
-        for asset_dict in asset_list:
-            # TODO: JSON returned does not match documentation at
-            # http://groups.google.com/group/dropio-api/web/resource-descriptions
-            yield Asset(asset_dict.get('name'), asset_dict.get('type'),
-                        asset_dict.get('title'), asset_dict.get('description'),
-                        asset_dict.get('filesize'), asset_dict.get('created_at')
-                        )
+        json_encoded = StringIO(f.read())
+        json_decoded = json.load(json_encoded)
+        for asset_dict in json_decoded:
+            a = Asset(asset_dict.get('url_fragment'),
+                      asset_dict.get('asset_type'),
+                      asset_dict.get('title'), 
+                      asset_dict.get('description'),
+                      asset_dict.get('file_size'), 
+                      asset_dict.get('created_at')                        
+                      )
+            yield a
     
     def get_drop(self, drop_name):
+        """
+        Returns:
+            dropio.resource.Drop
+        """
         assert drop_name is not None
         
         url = BASE_URL + DROPS + drop_name + '?' + self.urlencoded_params
+        f = self.__urlopen(url)
+        json_encoded = StringIO(f.read())
+        json_decoded = json.load(json_encoded)
+        
+        d = Drop(json_decoded.get('url'),
+                 json_decoded.get('email'),
+                 json_decoded.get('voicemail'),
+                 json_decoded.get('conference'),
+                 json_decoded.get('fax_coverpage_url'),
+                 json_decoded.get('rss'),
+                 'FIXME: where is asset count?')
+        return d
+
+    def __urlopen(self, url):
         try:
             f = urllib2.urlopen(url)
-            json_encoded = StringIO(f.read())
-            drop_dict = json.load(json_encoded)
         except IOError:
             raise
-        
-        return Drop(drop_dict['url'], drop_dict['id'])
+        return f
