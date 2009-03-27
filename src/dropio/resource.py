79751ba02f99124cdc881f0d145d1e16d5dbec22
Jimmy Orr <jimmyorr@gmail.com>
2009-03-27 03:46:09 +0000

    Added additional fields

--- src/dropio/resource.py
+++ src/dropio/resource.py
@@ -1,28 +1,39 @@
+#!/usr/bin/env python
+
+""" 
+Based on http://groups.google.com/group/dropio-api/web/resource-descriptions
+"""
+
+__author__ = 'jimmyorr@gmail.com (Jimmy Orr)'
+__version__ = '$Id$'
+
 class Resource(object):
     def __init__(self):
         pass
 
 
 class Drop(Resource):
-    def __init__(self, url, id):
+    def __init__(self, name=None, email=None, voicemail=None, conference=None, 
+                 fax=None, rss=None, asset_count=None, guest_token=None, 
+                 admin_token=None, expiration_length=None, 
+                 guests_can_comment=None, guests_can_add=None, 
+                 guests_can_delete=None, max_bytes=None, current_bytes=None, 
+                 hidden_upload_url=None):
         Resource.__init__(self)
-        self.url = url
-        self.id = id
+        self.name = name
+        self.email = email
+        self.voicemail = voicemail
+        self.conference = conference
+        self.fax = fax
+        self.rss = rss
     
     def __str__(self):
-        return self.url
+        return self.name
 
 
 class Asset(Resource):
-    """ 
-    Based on http://groups.google.com/group/dropio-api/web/resource-descriptions
-    (wrong)
-    """
-    def __init__(self, name, type, title, description, filesize, created_at,
-                 thumbnail=None, status=None, file=None, converted=None, 
-                 hidden_url=None, pages=None, duration=None, artist=None, 
-                 track_title=None, height=None, width=None, contents=None, 
-                 url=None):
+    def __init__(self, name=None, type=None, title=None, description=None, 
+                 filesize=None, created_at=None):
         Resource.__init__(self)
         self.name = name
         self.type = type
@@ -33,3 +44,12 @@ class Asset(Resource):
     
     def __str__(self):
         return self.title
+
+# TODO: image, movie, audio, document, note, link, archive, and other
+class SubAsset(Asset):
+    def __init__(self):
+        # thumbnail=None, status=None, file=None, converted=None, 
+        # hidden_url=None, pages=None, duration=None, artist=None, 
+        # track_title=None, height=None, width=None, contents=None, 
+        # url=None
+        pass
