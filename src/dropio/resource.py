192c59c0f6361e56975a4be12336eea8e19451af
Jimmy Orr <jimmyorr@gmail.com>
2009-04-16 12:54:42 +0000

    Switched constructors from using named parameters to more generic **kwargs

--- src/dropio/resource.py
+++ src/dropio/resource.py
@@ -12,42 +12,45 @@ class Resource(object):
 
 
 class Drop(Resource):
-    def __init__(self, name=None, email=None, voicemail=None, conference=None, 
-                 fax=None, rss=None, asset_count=None, guest_token=None, 
-                 admin_token=None, expiration_length=None, 
-                 guests_can_comment=None, guests_can_add=None, 
-                 guests_can_delete=None, max_bytes=None, current_bytes=None, 
-                 hidden_upload_url=None):
+    def __init__(self, *args, **kwargs):
         Resource.__init__(self)
-        self.name = name
-        self.email = email
-        self.voicemail = voicemail
-        self.conference = conference
-        self.fax = fax
-        self.rss = rss
+        self.name = kwargs.get('name')
+        self.email = kwargs.get('email')
+        self.voicemail = kwargs.get('voicemail')
+        self.conference = kwargs.get('conference')
+        self.fax = kwargs.get('fax')
+        self.rss = kwargs.get('rss')
+        self.asset_count = kwargs.get('asset_count')
+        self.guest_token = kwargs.get('guest_token')
+        self.admin_token = kwargs.get('admin_token')
+        self.expiration_length = kwargs.get('expiration_length')
+        self.guests_can_comment = kwargs.get('guests_can_comment')
+        self.guests_can_add = kwargs.get('guests_can_add')
+        self.guests_can_delete = kwargs.get('guests_can_delete')
+        self.max_bytes = kwargs.get('max_bytes')
+        self.current_bytes = kwargs.get('current_bytes')
+        self.hidden_upload_url = kwargs.get('hidden_upload_url')
     
     def __str__(self):
         return self.name
 
 
 class Asset(Resource):
-    def __init__(self, name=None, type=None, title=None, description=None, 
-                 filesize=None, created_at=None):
+    def __init__(self, *args, **kwargs):
         Resource.__init__(self)
-        self.name = name
-        self.type = type
-        self.title = title
-        self.description = description
-        self.filesize = filesize
-        self.created_at = created_at
+        self.name = kwargs.get('name')
+        self.type = kwargs.get('type')
+        self.title = kwargs.get('title')
+        self.description = kwargs.get('description')
+        self.filesize = kwargs.get('filesize')
+        self.created_at = kwargs.get('created_at')
     
     def __str__(self):
         return self.name
 
 
 class Link(Asset):
-    def __init__(self, name=None, type=None, title=None, description=None, 
-                 filesize=None, created_at=None, url=None):
-        Asset.__init__(self, name, type, title, description, filesize, created_at)
-        self.url = url
+    def __init__(self, *args, **kwargs):
+        Asset.__init__(self, args, kwargs)
+        self.url = kwargs.get('url')
     
