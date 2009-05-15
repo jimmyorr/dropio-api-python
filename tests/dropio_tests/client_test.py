f9d52891a2eeee77e232c986090ca5415b8e10b3
Jimmy Orr <jimmyorr@gmail.com>
2009-05-15 03:40:50 +0000

    Additional unit tests for get_drop

--- tests/dropio_tests/client_test.py
+++ tests/dropio_tests/client_test.py
@@ -7,9 +7,10 @@ import unittest
 import dropio.client
 
 api_key = ''
-drop_name = ''
+valid_drop_name = ''
+invalid_drop_name = 'zzzzzzzzzzzz'
 
-class DropIoClient(unittest.TestCase):
+class DropIoClientUnitTest(unittest.TestCase):
     
     def setUp(self):
         self.client = dropio.client.DropIoClient(api_key)
@@ -18,20 +19,36 @@ class DropIoClient(unittest.TestCase):
         # No teardown needed
         pass
     
-    def test_get_drop(self):
-        drop = self.client.get_drop(drop_name)
+    
+    #############
+    # get_drop()
+    #############
+    
+    def test_get_drop_valid_drop_name(self):
+        drop = self.client.get_drop(valid_drop_name)
         self.assert_(drop is not None)
-        self.assertEquals(drop.name, drop_name)
+        self.assertEquals(drop.name, valid_drop_name)
+    
+    def test_get_drop_invalid_drop_name(self):
+        self.assertRaises(Exception, self.client.get_drop, invalid_drop_name)
+        
+    def test_get_drop_None_drop_name(self):
+        self.assertRaises(Exception, self.client.get_drop, None)
+        
+    def test_get_drop_empty_drop_name(self):
+        self.assertRaises(Exception, self.client.get_drop, '')
+    
+    
+    ################
+    # create_link()
+    ################
     
     def test_create_link(self):
         link_url = 'http://foo.com'
-        link = self.client.create_link(drop_name, link_url)
+        link = self.client.create_link(valid_drop_name, link_url)
         self.assert_(link is not None)
         self.assertEquals(link.url, link_url)
-    
-    def test_get_asset_list(self):
-        # TODO:
-        pass
+
 
 if __name__ == '__main__':
     unittest.main()
