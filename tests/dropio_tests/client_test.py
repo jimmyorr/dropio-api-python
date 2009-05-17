f90d53cc503ff4863f4dd618e8e77a80a0ace3cd
Jimmy Orr <jimmyorr@gmail.com>
2009-05-17 06:44:22 +0000

    Some new tests...

--- tests/dropio_tests/client_test.py
+++ tests/dropio_tests/client_test.py
@@ -3,12 +3,13 @@
 __author__ = 'jimmyorr@gmail.com (Jimmy Orr)'
 
 import unittest
+import urllib2
 
 import dropio.client
 
 api_key = ''
 valid_drop_name = ''
-invalid_drop_name = 'zzzzzzzzzzzz'
+invalid_drop_name = '$$$'
 
 class DropIoClientUnitTest(unittest.TestCase):
     
@@ -40,6 +41,33 @@ class DropIoClientUnitTest(unittest.TestCase):
     
     
     ################
+    # update_drop()
+    ################
+    # FIXME: this is returning HTTPError: HTTP Error 500: Internal Server Error
+#===============================================================================
+#    def test_update_drop(self):
+#        drop = self.client.create_drop()
+#        self.assert_(drop is not None)
+#        self.assertEquals(drop.guests_can_delete, True)
+#        drop.guests_can_delete = False
+#        self.client.update_drop(drop, drop.admin_token)
+#        updated_drop = self.client.get_drop(drop.name, drop.admin_token)
+#        self.assertEquals(updated_drop.guests_can_delete, False)
+#===============================================================================
+    
+    
+    ################
+    # delete_drop()
+    ################
+    
+    def test_delete_drop(self):
+        drop = self.client.create_drop()
+        self.assert_(drop is not None)
+        self.client.delete_drop(drop.name, drop.admin_token)
+        self.assertRaises(urllib2.HTTPError, self.client.get_drop, drop.name, drop.admin_token)
+    
+    
+    ################
     # create_link()
     ################
     
@@ -48,7 +76,60 @@ class DropIoClientUnitTest(unittest.TestCase):
         link = self.client.create_link(valid_drop_name, link_url)
         self.assert_(link is not None)
         self.assertEquals(link.url, link_url)
+    
+    
+    ################
+    # create_note()
+    ################
+    
+    def test_create_note(self):
+        note_contents = 'blah blah blah'
+        note = self.client.create_note(valid_drop_name, note_contents)
+        self.assert_(note is not None)
+        self.assertEquals(note.contents, note_contents)
+    
+    
+    ##############
+    # get_asset()
+    ##############
+    
+    def test_get_asset(self):
+        note_contents = 'blah blah blah'
+        note = self.client.create_note(valid_drop_name, note_contents)
+        self.assert_(note is not None)
+        self.assertEquals(note.contents, note_contents)
+        note = self.client.get_asset(valid_drop_name, note.name)
+        self.assert_(note is not None)
+        self.assertEquals(note.contents, note_contents)
+    
+    
+    #################
+    # update_asset()
+    #################
+    
+    def test_update_link(self):
+        link_url = 'http://foo.com'
+        link = self.client.create_link(valid_drop_name, link_url)
+        self.assert_(link is not None)
+        link.url = 'http://bar.com'
+        link.title = 'this is a title'
+        link.description = 'this is a description'
+        self.client.update_asset(valid_drop_name, link)
+        
+        
+    #################
+    # delete_asset()
+    #################
+    
+    def test_delete_note(self):
+        note_contents = 'this note should be deleted'
+        note = self.client.create_note(valid_drop_name, note_contents)
+        self.assert_(note is not None)
+        self.assertEquals(note.contents, note_contents)
+        self.client.delete_asset(valid_drop_name, note.name)
 
 
 if __name__ == '__main__':
+    #unittest.main(DropIoClientUnitTest, 'test_update_drop')
     unittest.main()
+    
